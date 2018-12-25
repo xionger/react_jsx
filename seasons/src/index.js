233ab246c1c036119e4d978353fc41d8a6e01235
xionger <xunhao.xiong@gmail.com>
2018-12-25 15:25:46 -0600

    render with conditions

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,23 +5,29 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {lat: null, long: null};
+        this.state = {lat: null, errorMessage: ''};
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({lat: position.coords.latitude, long: position.coords.longitude});
             },
-            err => console.log(err)
+            err => {
+                this.setState({errorMessage: err.message});
+            }
         );
     }
 
     render() {
-        return (
-            <div>
+        if(this.state.errorMessage && !this.state.lat){
+            return (
+                <div>Error: {this.state.errorMessage}</div>
+            );
+        }else{
+            return (
                 <div>Latitude: {this.state.lat}</div>
-                <div>Longitude: {this.state.long}</div>
-            </div>
-        );
+            );
+        }
+
     };
 
 }
