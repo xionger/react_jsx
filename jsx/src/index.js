59a6b7fdb060c355d80dc24b1f554c3863e9cc97
xionger <xunhao.xiong@gmail.com>
2018-12-24 20:05:27 -0600

    using 3rd-party library

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar"></img>
+                    <img alt="avatar" src={faker.image.avatar()}></img>
                 </a>
                 <div className="content">
                     <a href="/" className="author">Sam</a>
