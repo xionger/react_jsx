88cc4322febb79cb2ce11888cbf22d7e8d15abd1
xionger <xunhao.xiong@gmail.com>
2018-12-25 07:52:42 -0600

    init seasons project

---
+++ seasons/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>Which season do you like?</div>
+    );
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
