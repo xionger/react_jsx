455129ff2737cf55683fe2b1705b5ac2b98e3804
xionger <xunhao.xiong@gmail.com>
2018-12-25 09:14:03 -0600

    add component, class and state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return (
-        <div>Which season do you like?</div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {lat: null, long: null};
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({lat: position.coords.latitude, long: position.coords.longitude});
+            },
+            err => console.log(err)
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <div>Latitude: {this.state.lat}</div>
+                <div>Longitude: {this.state.long}</div>
+            </div>
+        );
+    };
+
 }
 
 
