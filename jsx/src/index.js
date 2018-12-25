c9f04e3ab8661f491e4901be8ebeeda91af77554
xionger <xunhao.xiong@gmail.com>
2018-12-24 20:38:16 -0600

    add component

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,22 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 
+import CommentDetail from './CommentDetail';
+
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}></img>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">Sam</a>
-                    <div className="metadata">
-                        <span className="date">Today at 9:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
         </div>
     );
 };
