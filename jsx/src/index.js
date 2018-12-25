4e6bbe349058c9f66e2ca901ffeab647b5cb9232
xionger <xunhao.xiong@gmail.com>
2018-12-24 22:01:13 -0600

    props.children

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,30 +3,47 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail 
-                author="Sam" 
-                postTime="Today at 2:00AM" 
-                content="Hello World!"
-                avatar={faker.image.avatar()}/>
-            <CommentDetail 
-                author="Jack" 
-                postTime="Yesterday at 3:00PM" 
-                content="Good afternoon"
-                avatar={faker.image.avatar()}/>
-            <CommentDetail 
-                author="Calvin" 
-                postTime="Today at 6:35PM" 
-                content="It's sunny today"
-                avatar={faker.image.avatar()}/>
-            <CommentDetail 
-                author="Jane" 
-                postTime="Yesterday midnight" 
-                content="I'm sleepy!"
-                avatar={faker.image.avatar()}/>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    postTime="Today at 2:00AM" 
+                    content="Hello World!"
+                    avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <h3>Plain Text</h3>
+                <div>A card contains only text.</div>
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jack" 
+                    postTime="Yesterday at 3:00PM" 
+                    content="Good afternoon"
+                    avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Calvin" 
+                    postTime="Today at 6:35PM" 
+                    content="It's sunny today"
+                    avatar={faker.image.avatar()}/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    postTime="Yesterday midnight" 
+                    content="I'm sleepy!"
+                    avatar={faker.image.avatar()}/>
+            </ApprovalCard>
         </div>
     );
 };
