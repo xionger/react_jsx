40e1e4bb640d2063883e27dc4fd2596b39d33759
xionger <xunhao.xiong@gmail.com>
2018-12-25 19:41:16 -0600

    add spinner UI

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDesplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
     state = {lat: null, errorMessage: ''};
@@ -17,11 +18,15 @@ class App extends React.Component {
             return (
                 <div>Error: {this.state.errorMessage}</div>
             );
-        }else{
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
             return (
                 <SeasonDesplay lat={this.state.lat} />
             );
         }
+
+        return <Spinner message="Loading location ... "/>;
 
     };
 
