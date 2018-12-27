a20575528cb668b26bbc55b5f17a4bedd67e7695
xionger <xunhao.xiong@gmail.com>
2018-12-26 20:05:17 -0600

    add searchbar component

--- pictures/src/index.js
+++ pictures/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-    return <div>Hello World!</div>;
-}
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
