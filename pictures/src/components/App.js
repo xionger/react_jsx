72fbe6f51aadc50a8732c438e4c91bfe395f4ff8
xionger <xunhao.xiong@gmail.com>
2018-12-30 21:53:29 -0600

    create client

--- pictures/src/components/App.js
+++ pictures/src/components/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import axios from 'axios';
 
 import SearchBar from './SearchBar';
+import Unsplash from '../api/Unsplash';
 
 class App extends React.Component {
     state = {images: []};
 
     onSearchSubmit = async (term) => {
-        const response = await axios.get('https://api.unsplash.com/search/photos', {
-            params: {query: term},
-            headers: {
-                //Authorization: 'Client-ID ACCESS_KEY_HERE'
-                
-            }
+        const response = await Unsplash.get('/search/photos', {
+            params: {query: term}
         });
         
         this.setState({images: response.data.results});
