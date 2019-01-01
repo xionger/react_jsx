7a1beb5c139f2ef67c2605ff5b68d86408ca297f
xionger <xunhao.xiong@gmail.com>
2019-01-01 10:53:38 -0600

    dynamic layout

--- pictures/src/api/Unsplash.js
+++ pictures/src/api/Unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
         //Authorization: 'Client-ID ACCESS_KEY_HERE'
-        Authorization: 'Client-ID 619d22deb760101d03fcf02076eaf9e3f6db601fee35d781118aba9ecdb9fc11'
+   
     }
 });
